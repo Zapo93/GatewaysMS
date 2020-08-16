c452a29764424dc69f039aee8a397ffd29633163
zapryan.bekirski <zbekirski@abv.bg>
2020-08-16 22:47:14 +0300

    Added tests for correct behavior regarding adding more than and less than the maximum devices allowed in a gateway.

--- GatewaysAPI/Gateways.Tests/GatewaysManagerTests.cs
+++ GatewaysAPI/Gateways.Tests/GatewaysManagerTests.cs
@@ -16,6 +16,7 @@ namespace Gateways.Tests
     public class GatewaysManagerTests
     {
         private readonly string DBConnectionString = "Server=localhost;Database=ms_gateways;Trusted_Connection=True;";
+        private readonly int MaxNumberOfDevicesInGateway = 10;
 
         [TestMethod]
         public async Task CreateAndDeleteGateway_ExistingSerialNumber_ThrowsError()
@@ -73,6 +74,110 @@ namespace Gateways.Tests
             Assert.AreEqual(newDevice.Vendor, gatewayFromDB.Devices[0].Vendor);
             Assert.AreEqual(newDevice.DateCreated.ToString(), gatewayFromDB.Devices[0].DateCreated.ToString());
             Assert.AreEqual(newDevice.Status, gatewayFromDB.Devices[0].Status);
+        }
+
+        [TestMethod]
+        public async Task Add10DevicesToGateway_CreateGateway_Success()
+        {
+            IGatewaysDataAccess dataAccess = new GatewaysMSSQLDataAccess(DBConnectionString);
+            IGatewaysManager gatewaysManager = new GatewaysManager(dataAccess);
+
+            Gateway newGateway = MockGateway.Create();
+
+            for (int i = 0; i < MaxNumberOfDevicesInGateway; i++)
+            {
+                Device newDevice = MockDevice.Create();
+                newGateway.Devices.Add(newDevice);
+            }
+
+            await gatewaysManager.CreateGateway(newGateway);
+
+            await gatewaysManager.DeleteGatewayBySerialNumber(newGateway.SerialNumber);
+
+            await Assert.ThrowsExceptionAsync<GatewayDoesNotExistException>(async () =>
+            {
+                await gatewaysManager.GetGatewayStatus(newGateway.SerialNumber);
+            });
+        }
+
+        [TestMethod]
+        public async Task AddMoreThan10DevicesToGateway_CreateGateway_CorrespondingErrorThrown()
+        {
+            IGatewaysDataAccess dataAccess = new GatewaysMSSQLDataAccess(DBConnectionString);
+            IGatewaysManager gatewaysManager = new GatewaysManager(dataAccess);
+
+            Gateway newGateway = MockGateway.Create();
+
+            for (int i=0; i<= MaxNumberOfDevicesInGateway; i++) 
+            {
+                Device newDevice = MockDevice.Create();
+                newGateway.Devices.Add(newDevice);
+            }
+
+            await Assert.ThrowsExceptionAsync<MaximumDevicesExceededException>(async () =>
+            {
+                await gatewaysManager.CreateGateway(newGateway);
+            });
+
+            await gatewaysManager.DeleteGatewayBySerialNumber(newGateway.SerialNumber);
+
+            await Assert.ThrowsExceptionAsync<GatewayDoesNotExistException>(async () =>
+            {
+                await gatewaysManager.GetGatewayStatus(newGateway.SerialNumber);
+            });
+        }
+
+        [TestMethod]
+        public async Task Add10DevicesToGateway_AddDevice_Success()
+        {
+            IGatewaysDataAccess dataAccess = new GatewaysMSSQLDataAccess(DBConnectionString);
+            IGatewaysManager gatewaysManager = new GatewaysManager(dataAccess);
+
+            Gateway newGateway = MockGateway.Create();
+
+            for (int i = 0; i < MaxNumberOfDevicesInGateway - 1; i++)
+            {
+                Device newDevice = MockDevice.Create();
+                newGateway.Devices.Add(newDevice);
+            }
+
+            await gatewaysManager.CreateGateway(newGateway);
+            await gatewaysManager.AddDeviceToGateway(newGateway.SerialNumber, MockDevice.Create());
+            await gatewaysManager.DeleteGatewayBySerialNumber(newGateway.SerialNumber);
+
+            await Assert.ThrowsExceptionAsync<GatewayDoesNotExistException>(async () =>
+            {
+                await gatewaysManager.GetGatewayStatus(newGateway.SerialNumber);
+            });
+        }
+
+        [TestMethod]
+        public async Task AddMoreThan10DevicesToGateway_AddDevice_CorrespondingErrorThrown()
+        {
+            IGatewaysDataAccess dataAccess = new GatewaysMSSQLDataAccess(DBConnectionString);
+            IGatewaysManager gatewaysManager = new GatewaysManager(dataAccess);
+
+            Gateway newGateway = MockGateway.Create();
+
+            for (int i = 0; i < MaxNumberOfDevicesInGateway; i++)
+            {
+                Device newDevice = MockDevice.Create();
+                newGateway.Devices.Add(newDevice);
+            }
+
+            await gatewaysManager.CreateGateway(newGateway);
+
+            await Assert.ThrowsExceptionAsync<MaximumDevicesExceededException>(async () =>
+            {
+                await gatewaysManager.AddDeviceToGateway(newGateway.SerialNumber,MockDevice.Create());
+            });
+
+            await gatewaysManager.DeleteGatewayBySerialNumber(newGateway.SerialNumber);
+
+            await Assert.ThrowsExceptionAsync<GatewayDoesNotExistException>(async () =>
+            {
+                await gatewaysManager.GetGatewayStatus(newGateway.SerialNumber);
+            });
         }
 
 
